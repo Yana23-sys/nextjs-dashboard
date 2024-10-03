031c81367169bf68a4d95cd5462a1e8b0c04fb64
Yana Turiak <turyak1998@gmail.com>
2024-10-03 15:54:07 +0100

    create an invoice

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,10 +7,17 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { createInvoice } from '@/app/lib/actions';
+
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+
+
+  // In HTML, I'd pass a URL (destination where form data should be submitted (usually API endpoint))
+  // But in React action attribute is a special prop -> Behind the scenes, Server Actions create a POST API endpoint. This is why we don't need to create API endpoints manually when using Server Actions.
   return (
-    <form>
+    <form action={createInvoice}>
+
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
